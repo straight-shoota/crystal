ceba7caade3b4638a40430e74e60857a1e5ecacf
Johannes Müller <straightshoota@gmail.com>
2021-11-04 19:47:20 +0100

    wip

--- src/exception/call_stack/elf.cr
+++ src/exception/call_stack/elf.cr
@@ -19,35 +19,16 @@ struct Exception::CallStack
     program = Process.executable_path
     return unless program && File.readable? program
     Crystal::ELF.open(program) do |elf|
-      line_strings = elf.read_section?(".debug_line_str") do |sh, io|
-        Crystal::DWARF::Strings.new(io, sh.offset, sh.size)
+      dwarf = Crystal::DWARF::Data.new(elf)
+
+      @@dwarf_line_numbers = dwarf.line_numbers
+
+      names = [] of {LibC::SizeT, LibC::SizeT, String}
+
+      parse_function_names_from_dwarf(dwarf.info, dwarf.strings, dwarf.line_strings) do |low_pc, high_pc, name|
+        names << {low_pc + base_address, high_pc + base_address, name}
       end
-
-      strings = elf.read_section?(".debug_str") do |sh, io|
-        Crystal::DWARF::Strings.new(io, sh.offset, sh.size)
-      end
-
-      elf.read_section?(".debug_line") do |sh, io|
-        @@dwarf_line_numbers = Crystal::DWARF::LineNumbers.new(io, sh.size, base_address, strings, line_strings)
-      end
-
-      elf.read_section?(".debug_info") do |sh, io|
-        names = [] of {LibC::SizeT, LibC::SizeT, String}
-
-        while (offset = io.pos - sh.offset) < sh.size
-          info = Crystal::DWARF::Info.new(io, offset)
-
-          elf.read_section?(".debug_abbrev") do |sh, io|
-            info.read_abbreviations(io)
-          end
-
-          parse_function_names_from_dwarf(info, strings, line_strings) do |low_pc, high_pc, name|
-            names << {low_pc + base_address, high_pc + base_address, name}
-          end
-        end
-
-        @@dwarf_function_names = names
-      end
+      @@dwarf_function_names = names
     end
   end
 
