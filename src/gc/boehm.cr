ba921101ca167d22d466fb6c1566a7fb703fb4cf
Johannes Müller <straightshoota@gmail.com>
2024-01-10 14:30:09 +0100

    Remove `pthread` link from boehm.cr

--- src/gc/boehm.cr
+++ src/gc/boehm.cr
@@ -2,23 +2,6 @@
   require "crystal/rw_lock"
 {% end %}
 
-# MUSL: On musl systems, libpthread is empty. The entire library is already included in libc.
-# The empty library is only available for POSIX compatibility. We don't need to link it.
-#
-# Darwin: `libpthread` is provided as part of `libsystem`. There's no reason to link it explicitly.
-#
-# Interpreter: Starting with glibc 2.34, `pthread` is integrated into `libc`
-# and may not even be available as a separate shared library.
-# There's always a static library for compiled mode, but `Crystal::Loader` does not support
-# static libraries. So we just skip `pthread` entirely. The symbols are still
-# available in the interpreter because they are loaded in the compiler.
-#
-# OTHERS: On other systems, we add the linker annotation here to make sure libpthread is loaded
-# before libgc which looks up symbols from libpthread.
-{% unless flag?(:win32) || flag?(:musl) || flag?(:darwin) || flag?(:android) || (flag?(:interpreted) && flag?(:gnu)) %}
-  @[Link("pthread")]
-{% end %}
-
 {% if flag?(:freebsd) || flag?(:dragonfly) %}
   @[Link("gc-threaded")]
 {% elsif flag?(:interpreted) %}
