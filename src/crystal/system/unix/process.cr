407d04f8db0b8c8e1f6cdd1c87885112ece112da
Johannes Müller <straightshoota@gmail.com>
2025-11-15 23:32:47 +0100

    Drop `Process.fork`

--- src/crystal/system/unix/process.cr
+++ src/crystal/system/unix/process.cr
@@ -192,7 +192,7 @@ struct Crystal::System::Process
     end
   {% end %}
 
-  def self.fork(*, will_exec : Bool, &)
+  def self.fork
     newmask = uninitialized LibC::SigsetT
     oldmask = uninitialized LibC::SigsetT
 
@@ -212,8 +212,7 @@ struct Crystal::System::Process
       # child:
       pid = nil
 
-      # after fork callback
-      yield
+      Crystal::System::Signal.after_fork_before_exec
 
       if will_exec
         # reset sigmask (inherited on exec)
@@ -236,29 +235,6 @@ struct Crystal::System::Process
     pid
   end
 
-  # Duplicates the current process.
-  # Returns a `Process` representing the new child process in the current process
-  # and `nil` inside the new child process.
-  def self.fork(&)
-    {% raise("Process fork is unsupported with multithreaded mode") if flag?(:preview_mt) %}
-
-    pid = fork(will_exec: false) do
-      ::Process.after_fork_child_callbacks.each(&.call)
-    end
-    return pid if pid
-
-    begin
-      yield
-      LibC._exit 0
-    rescue ex
-      ex.inspect_with_backtrace STDERR
-      STDERR.flush
-      LibC._exit 1
-    ensure
-      LibC._exit 254 # not reached
-    end
-  end
-
   def self.spawn(command, args, shell, env, clear_env, input, output, error, chdir)
     prepared_args = prepare_args(command, args, shell)
 
@@ -266,9 +242,7 @@ struct Crystal::System::Process
 
     envp = Env.make_envp(env, clear_env)
 
-    pid = fork(will_exec: true) do
-      Crystal::System::Signal.after_fork_before_exec
-    end
+    pid = fork
 
     if !pid
       LibC.close(r)
